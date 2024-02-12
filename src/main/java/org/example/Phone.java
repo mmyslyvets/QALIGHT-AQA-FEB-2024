5dc301d8432481a9355d2ba842484758a0dd38c5
Max Myslywets <maksym.myslyvets@pitchbook.com>
2024-02-12 14:17:38 +0200

    refactoring

--- src/main/java/org/example/Phone.java
+++ src/main/java/org/example/Phone.java
@@ -2,23 +2,40 @@ package org.example;
 
 public class Phone {
 
-    public String model_name;
+    public String model_name = "";
 
-    public int charge_level;
+    public int charge_level = 0;
 
-    public void charging(){
-       if (charge_level < 80){
-           charge_level=charge_level + 10;
-       } else if (charge_level >= 80 || charge_level <= 100) {
-           charge_level = charge_level + 1;
-       }
+    public boolean isFullChange() {
+        return charge_level == 100;
     }
 
-    public void discharging(){
-        if (charge_level > 0){
-            charge_level = charge_level - 1;
-        }
+    public boolean isDischarged() {
+        return charge_level == 10;
+    }
 
+    public boolean isNeedCharge() {
+        return charge_level < 100;
+    }
+
+    public boolean canDischarging() {
+        return charge_level > 10;
+    }
+
+    public void charging() {
+        boolean hasSuperCharge = charge_level < 80;
+        if (hasSuperCharge) {
+            incLvl(10);
+        } else {
+            incLvl(1);
+        }
+    }
+
+    public void discharging() {
+        if (charge_level <= 0)
+            return;
+
+        charge_level = charge_level - 1;
     }
 
     private void incLvl(int incLevel) {

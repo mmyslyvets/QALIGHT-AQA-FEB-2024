71ff96b62a96de101ffd2ce2eba1b399e2ea89b0
Max Myslywets <maksym.myslyvets@pitchbook.com>
2024-02-19 16:48:56 +0200

    Refactoring

--- src/main/java/org/sorting/SortingGnome.java
+++ src/main/java/org/sorting/SortingGnome.java
@@ -1,11 +1,13 @@
 package org.sorting;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Random;
 
 public class SortingGnome {
 
     public static void main(String[] args) {
-        int[] gnome = new int[30];
+        int[] gnome = new int[40];
         fillGnomes(gnome);
         System.out.println("_____");
         sortGnomes(gnome);
@@ -13,21 +15,21 @@ public class SortingGnome {
 
     }
 
-    private static void fillGnomes(int[] gnome) {
+    private static void fillGnomes(int @NotNull [] gnome) {
         Random random = new Random();
 
         for (int i = 0; i < gnome.length; i++) {
-            gnome[i] = random.nextInt(30);
+            gnome[i] = random.nextInt(100);
             System.out.println(gnome[i]);
         }
     }
 
-    private static void printSortedArray(int[] gnome) {
+    private static void printSortedArray(int @NotNull [] gnome) {
         for (int i = 0; i < gnome.length; i++)
             System.out.println(gnome[i]);
     }
 
-    private static void sortGnomes(int[] gnome) {
+    private static void sortGnomes(int @NotNull [] gnome) {
         for (int i = 0; i < gnome.length; i++) {
             if(i == 0){
                 continue;

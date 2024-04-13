cdc3c6ee4c8d10d27c0b154524ec08b0c7e1c827
aosipenko <aoskiev@gmail.com>
2024-04-13 09:50:37 +0200

    session 18: Jenkins Selenoid Docker images + setup

--- src/main/java/org/collections/web/driver/WebDriverFactory.java
+++ src/main/java/org/collections/web/driver/WebDriverFactory.java
@@ -1,23 +1,26 @@
 package org.collections.web.driver;
 
+import lombok.SneakyThrows;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
 
 public class WebDriverFactory {
 
+    @SneakyThrows
     public static WebDriver getDriver() {
-        String driverType = System.getenv().get("driver.type");
         String driverTypeFromProps = System.getProperty("driver.type", "CHROME");
-        String value;
-        if (driverType != null) {
-            value = driverType;
-        } else {
-            value = driverTypeFromProps;
-        }
-        switch (value) {
+
+        switch (driverTypeFromProps) {
+            case "JENKINS":
+                return new RemoteWebDriver(
+                        new URL("http://localhost:4444/wb/hub"), remoteOptions());
             case "EDGE":
                 return new EdgeDriver();
             case "FIREFOX":
@@ -28,6 +31,13 @@ public class WebDriverFactory {
     }
 
     private static ChromeOptions chromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.setAcceptInsecureCerts(true);
+        options.addArguments("--start-maximized");
+        return options;
+    }
+
+    private static Capabilities remoteOptions() {
         ChromeOptions options = new ChromeOptions();
         options.setAcceptInsecureCerts(true);
         options.addArguments("--start-maximized");
